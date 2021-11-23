42e779279246d14d228cacac43bc6defbb547a89
Nicolas Bernier <nbernier99@gmail.com>
2021-11-22 20:40:51 -0500

    task 1.1 done

--- src/Task 1.py
+++ src/Task 1.py
@@ -1,4 +1,6 @@
+import csv
 import gensim.downloader as api
+from gensim.models import Word2Vec
 
 wv = api.load('word2vec-google-news-300')
 
@@ -7,6 +9,7 @@ for index, word in enumerate(wv.index_to_key):
         break
     print(f"word #{index}/{len(wv.index_to_key)} is {word}")
 
+#  cosine similarity between pairs of embeddings
 pairs = [
     ('car', 'minivan'),  # a minivan is a kind of car
     ('car', 'bicycle'),  # still a wheeled vehicle
@@ -17,3 +20,39 @@ pairs = [
 
 for w1, w2 in pairs:
     print('%r\t%r\t%.2f' % (w1, w2, wv.similarity(w1, w2)))
+
+# Task 1.1
+
+synonyms_csv = open('../data/synonyms.csv', mode='r')
+
+with open('task1.1_output.csv', 'w', newline='') as output_file:
+    output_writer = csv.writer(output_file)
+    parsed_file = csv.reader(synonyms_csv, delimiter=',')
+    for line in parsed_file:
+        question = line[0]
+        answer = line[1]
+
+        # (word, score)
+        label = None
+        prediction = [None, 0]
+        try:
+            for i in range(2, 6):
+                score = wv.similarity(question, line[i])
+                if score > prediction[1]:
+                    prediction = [line[i], score]
+            if prediction[0] == answer:
+                label = 'correct'
+            else:
+                label = 'wrong'
+        except:
+            label = "guess"
+
+            # Guessing first when word not found
+            prediction[0] = line[2]
+
+        row = [question, answer, prediction[0], label]
+        output_writer.writerow(row)
+
+synonyms_csv.close()
+output_file.close()
+
