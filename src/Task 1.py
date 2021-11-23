e40b8c75129f509f08df53286d05f0e3edbeec1e
Nicolas Bernier <nbernier99@gmail.com>
2021-11-23 16:09:27 -0500

    task 1.2 done

--- src/Task 1.py
+++ src/Task 1.py
@@ -23,17 +23,19 @@ for w1, w2 in pairs:
 
 # Task 1.1
 
+total_correct = 0
+total_answered = 0
 synonyms_csv = open('../data/synonyms.csv', mode='r')
 
-with open('task1.1_output.csv', 'w', newline='') as output_file:
+with open('word2vec-google-news-300-details.csv', 'w', newline='') as output_file:
     output_writer = csv.writer(output_file)
     parsed_file = csv.reader(synonyms_csv, delimiter=',')
     for line in parsed_file:
         question = line[0]
         answer = line[1]
 
-        # (word, score)
         label = None
+        # (word, score) => 81 instances in test file
         prediction = [None, 0]
         try:
             for i in range(2, 6):
@@ -41,9 +43,11 @@ with open('task1.1_output.csv', 'w', newline='') as output_file:
                 if score > prediction[1]:
                     prediction = [line[i], score]
             if prediction[0] == answer:
+                total_correct += 1
                 label = 'correct'
             else:
                 label = 'wrong'
+            total_answered += 1
         except:
             label = "guess"
 
@@ -53,6 +57,24 @@ with open('task1.1_output.csv', 'w', newline='') as output_file:
         row = [question, answer, prediction[0], label]
         output_writer.writerow(row)
 
+# Task 1.2
+
+with open('analysis.csv', 'w', newline='') as analysis_file:
+    analysis_writer = csv.writer(analysis_file)
+
+    vocab = wv.index_to_key
+    accuracy = total_correct/total_answered
+
+    row = []
+    row.append("word2vec-google-news-300")
+
+    row.append(len(vocab))
+    row.append(total_correct)
+    row.append(total_answered)
+    row.append(accuracy)
+    analysis_writer.writerow(row)
+
+
 synonyms_csv.close()
 output_file.close()
-
+analysis_file.close()
